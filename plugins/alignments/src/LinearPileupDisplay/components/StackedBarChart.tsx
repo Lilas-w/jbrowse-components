d3ce56f47ddd174cb3f13a41e8c921b65d9211de
Lilas-w <wangziweipku@pku.edu.cn>
2023-06-26 14:09:12 +0800

    fix the chart display error

--- plugins/alignments/src/LinearPileupDisplay/components/StackedBarChart.tsx
+++ plugins/alignments/src/LinearPileupDisplay/components/StackedBarChart.tsx
@@ -1,96 +1,101 @@
-import React, { useEffect, useRef } from 'react'
-import { observer } from 'mobx-react'
-import { Dialog, DialogContent, Typography } from '@mui/material'
+import React, { useEffect, useRef } from 'react';
+import { observer } from 'mobx-react';
+import { Dialog, DialogContent, Typography } from '@mui/material';
 import * as echarts from 'echarts';
 import { StackedBarChartData } from '../../PileupRenderer/StackedBarChartData';
 
 function StackedBarChartDlg(props: { model: any; handleClose: () => void }) {
-  const { model, handleClose } = props
-  const chartRef = useRef<HTMLDivElement | null>(null)
+  const { model, handleClose } = props;
+  const chartRef = useRef<HTMLDivElement | null>(null);
   console.log(StackedBarChartData.seriesData);
 
   useEffect(() => {
     if (chartRef.current) {
-      const chart = echarts.init(chartRef.current)
+      const chart = echarts.init(chartRef.current);
 
-      const option = {
+      const option: echarts.EChartOption<echarts.EChartOption.Series> = {
         tooltip: {
           trigger: 'axis',
           axisPointer: {
-            type: 'shadow'
-          }
+            type: 'shadow',
+          },
         },
         legend: {},
         grid: {
           left: '3%',
           right: '4%',
           bottom: '3%',
-          containLabel: true
+          containLabel: true,
         },
-        xAxis: [
-            {
-              type: 'category',
-              data: ['TF1', 'TF2'],
-            },
-          ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
+        xAxis: {
+          type: 'category',
+          data: ['TF1', 'TF2'],
+        },
+        yAxis: {
+          type: 'value',
+        },
         series: [
           {
             name: 'R11',
             type: 'bar',
             stack: 'Ad',
             emphasis: {
-              focus: 'series'
+              itemStyle: {
+                opacity: 1,
+              },
             },
-            data: [0.31, 0.31]
+            data: [0.31, 0.31],
           },
           {
             name: 'R10',
             type: 'bar',
             stack: 'Ad',
             emphasis: {
-              focus: 'series'
+              itemStyle: {
+                opacity: 1,
+              },
             },
-            data: [0.25, 0.25]
+            data: [0.25, 0.25],
           },
           {
             name: 'R01',
             type: 'bar',
             stack: 'Ad',
             emphasis: {
-              focus: 'series'
+              itemStyle: {
+                opacity: 1,
+              },
             },
-            data: [0, 0]
+            data: [0, 0],
           },
           {
             name: 'R00',
             type: 'bar',
             stack: 'Ad',
             emphasis: {
-              focus: 'series'
+              itemStyle: {
+                opacity: 1,
+              },
             },
-            data: [0.44, 0.44]
-          }
-        ]
-      }
+            data: [0.44, 0.44],
+          },
+        ],
+      };
 
-      chart.setOption(option)
-      window.addEventListener('resize', () => {
-        chart.resize()
-      })
+      chart.setOption(option);
 
+      const resizeListener = () => {
+        chart.resize();
+      };
+  
+      window.addEventListener('resize', resizeListener);
+  
       return () => {
-        chart.dispose()
-        window.removeEventListener('resize', () => {
-          chart.resize()
-        })
-      }
+        chart.dispose();
+        window.removeEventListener('resize', resizeListener);
+      };
     }
-  }, [])
+  }, []);
 
   return (
     <Dialog open onClose={handleClose} title="Show stacked bar">
@@ -99,7 +104,7 @@ function StackedBarChartDlg(props: { model: any; handleClose: () => void }) {
         <div ref={chartRef} style={{ height: '300px' }} />
       </DialogContent>
     </Dialog>
-  )
+  );
 }
 
-export default observer(StackedBarChartDlg)
+export default observer(StackedBarChartDlg);
